4c1a9c63d918301a7ae80f6802709391c8f64be5
chrisashe00 <chrisashe60@gmail.com>
2024-12-03 23:03:56 +0000

    idek

--- script.js
+++ script.js
@@ -1,12 +1,98 @@
-document
-  .getElementsByClassName("footer-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting
-    var element = document.getElementsByClassName("newsletter-confirm");
-    // Change display from 'none' to 'block'
-    if (element.style.display === "none" || element.style.display === "") {
-      element.style.display = "block";
-    } else {
-      element.style.display = "none"; // Optional: Toggle back to none
-    }
-  });
+// Get modal element
+var modal = document.getElementById("simpleModal");
+
+// Get hamburger element
+var hamburger = document.getElementById("hamburger");
+
+// get aside
+var aside = document.getElementById("aside");
+
+// Ge email input Div
+var email = document.getElementById("emailInput");
+
+// get error message
+var error = document.getElementById("errorMsg");
+
+// get aside wrapper
+var asideWrapper = document.getElementById("aside-wrapper");
+
+// get aside close button
+var closeAsideBtn = document.getElementsByClassName("closeAsideBtn")[0];
+
+// Get open modal button
+var modalBtn = document.getElementById("modalBtn");
+
+// Get close button
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+// Get newsletter confirmation div
+var newsletter = document.getElementById("newsletter-confirm");
+
+// Get newsletter button
+var footerForm = document.getElementById("footer-form");
+
+// Get news button div
+var newsBtn = document.getElementById("news-button");
+
+// listen for open click
+modalBtn.addEventListener("click", openModal);
+
+// Listen for hamburger click
+hamburger.addEventListener("click", openAside);
+
+// listen for close click
+closeBtn.addEventListener("click", closeModal);
+
+// listen for Outside Click
+window.addEventListener("click", outsideClick);
+
+// Listen for close aside click
+closeAsideBtn.addEventListener("click", closeAside);
+
+// function to close aside
+function openAside() {
+  asideWrapper.style.display = "none";
+}
+
+// Function to open aside
+function openAside() {
+  asideWrapper.style.display = "block";
+}
+
+// function to close aside
+function closeAside() {
+  asideWrapper.style.display = "none";
+}
+
+// Function to open modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Function to close modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function newsDisplay() {
+  "running";
+  if (email.value == "") {
+    console.log("not a valid email");
+    error.style.display = "block";
+    setTimeout(() => error.remove(), 3000);
+  } else {
+    newsletter.style.display = "block";
+    footerForm.style.display = "none";
+  }
+}
+
+// Function to close modal if outside click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+
+  if (e.target == asideWrapper) {
+    asideWrapper.style.display = "none";
+  }
+}
